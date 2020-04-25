7411c5e54fa55b6e242f92a1ec9cdbf74cc54ff5
Teodor Tenchev <teodortenchev@gmail.com>
2020-04-25 17:05:01 +0400

    Added SchoolAppUser.cs

---
+++ src/Data/DubaiMusicSchool.Data.Models/SchoolAppUser.cs
@@ -0,0 +1,24 @@
+﻿namespace DubaiMusicSchool.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using DubaiMusicSchool.Common;
+
+    public class SchoolAppUser : ApplicationUser
+    {
+        [MaxLength(GlobalConstants.PersonNameMaxLength)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [MaxLength(GlobalConstants.PersonNameMaxLength)]
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+    }
+}
--- src/DubaiMusicSchool.Common/GlobalConstants.cs
+++ src/DubaiMusicSchool.Common/GlobalConstants.cs
@@ -5,5 +5,9 @@
         public const string SystemName = "DubaiMusicSchool";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const int PersonNameMaxLength = 35;
+
+        public const int HomeAddressMaxLength = 150;
     }
 }
