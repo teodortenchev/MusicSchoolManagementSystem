c5dde332792e57a63586b53ecb1f1ed397a0c081
Teodor Tenchev <teodortenchev@gmail.com>
2020-04-25 20:04:17 +0400

    Configured bootstrap template

--- src/DubaiMusicSchool.Common/GlobalConstants.cs
+++ src/DubaiMusicSchool.Common/GlobalConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class GlobalConstants
     {
-        public const string SystemName = "DubaiMusicSchool";
+        public const string SystemName = "Dubai Music School";
 
         public const string AdministratorRoleName = "Administrator";
 
