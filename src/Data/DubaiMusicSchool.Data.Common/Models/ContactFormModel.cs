15189315a55b58d972e9eb4387da9871c5dbe126
Teodor Tenchev <teodortenchev@gmail.com>
2020-04-30 18:28:44 +0400

    contact form model and recaptcha

---
+++ src/Data/DubaiMusicSchool.Data.Common/Models/ContactFormModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DubaiMusicSchool.Data.Common.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ContactFormModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        public string ReCaptcha { get; set; }
+    }
+}
---
+++ src/Data/DubaiMusicSchool.Data.Common/Models/ReCaptcha.cs
@@ -0,0 +1,13 @@
+﻿namespace DubaiMusicSchool.Data.Common.Models
+{
+    public class ReCaptcha
+    {
+        public string Success { get; set; }
+
+        public string Score { get; set; }
+
+        public string ChallengeTs { get; set; }
+
+        public string HostName { get; set; }
+    }
+}
